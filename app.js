53ccadd59cffd97f35e6263fec6b331346339357
jfcarocota <jfcarocota@gmail.com>
2019-11-03 11:37:53 -0800

    shader compile
    la compilación de shaders esta funcionando correctamente

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
 
 const canvas = document.getElementById('glcanvas');
 
+const vsSource = `
+    attribute vec4 aVertexPosition;
+
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    void main() {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+  `;
+
+const fsSource = `
+  void main() {
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  }
+`;
+
+const loadShader = (gl, type, source)=>{
+    const shader = gl.createShader(type);
+
+    gl.shaderSource(shader, source);
+
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
+
+const initShaderProgram = (gl, vsSource, fsSource)=>{
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    
+}
+
 const main = ()=>{
     gl = canvas.getContext('webgl2');
 
@@ -8,6 +48,8 @@ const main = ()=>{
         alert('tu navegador no soporta webgl2');
         return;
     }
+
+    initShaderProgram(gl, vsSource, fsSource);
 
     gl.clearColor(1, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
