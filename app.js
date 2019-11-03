e7e35551c6683a340742b75156840dcb35ea7ad6
jfcarocota <jfcarocota@gmail.com>
2019-11-03 12:41:44 -0800

    dibujar cuadrado
    proyeccion y vista de la matrix terminado

--- app.js
+++ app.js
@@ -38,7 +38,112 @@ const initShaderProgram = (gl, vsSource, fsSource)=>{
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
-    
+    const shaderProgram = gl.createProgram();
+
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return null;
+    }
+
+    return shaderProgram;
+}
+
+const initBuffer = gl =>{
+    const positionBuffer = gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    const positions = [
+        -1,  1,
+         1,  1,
+        -1, -1,
+         1, -1,
+      ];
+
+    gl.bufferData(
+        gl.ARRAY_BUFFER, 
+        new Float32Array(positions), 
+        gl.STATIC_DRAW
+    );
+
+    return{
+        position : positionBuffer
+    }
+}
+
+const drawScene = (gl, programInfo, buffers)=>{
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+    gl.clearDepth(1.0);                 // Clear everything
+    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+  
+    // Clear the canvas before we start drawing on it.
+  
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const fieldOfView = 45 * Math.PI / 180;   // in radians
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+    const projectionMatrix = mat4.create();
+
+    mat4.perspective(
+        projectionMatrix,
+        fieldOfView,
+        aspect,
+        zNear,
+        zFar
+    );
+
+    const modelViewMatrix = mat4.create();
+
+    mat4.translate(modelViewMatrix,     // destination matrix
+        modelViewMatrix,     // matrix to translate
+        [-0.0, 0.0, -6.0]);  // amount to translate
+
+        {
+            const numComponents = 2;  // pull out 2 values per iteration
+            const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+            const normalize = false;  // don't normalize
+            const stride = 0;         // how many bytes to get from one set of values to the next
+                                      // 0 = use type and numComponents above
+            const offset = 0;         // how many bytes inside the buffer to start from
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+
+            gl.vertexAttribPointer(
+                programInfo.attribLocations.vertexPosition,
+                numComponents,
+                type,
+                normalize,
+                stride,
+                offset
+            );
+
+            gl.enableVertexAttribArray(
+                programInfo.attribLocations.vertexPosition);
+        }
+
+        gl.useProgram(programInfo.program);
+        gl.uniformMatrix4fv( programInfo.uniformLocations.projectionMatrix, 
+            false, 
+            projectionMatrix
+        );
+        gl.uniformMatrix4fv(
+            programInfo.uniformLocations.modelViewMatrix,
+            false,
+            modelViewMatrix
+        );
+
+        {
+            const offset = 0;
+            const vertexCount = 4;
+            gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+        }
 }
 
 const main = ()=>{
@@ -49,10 +154,22 @@ const main = ()=>{
         return;
     }
 
-    initShaderProgram(gl, vsSource, fsSource);
+    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
 
-    gl.clearColor(1, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    const programInfo = {
+        program : shaderProgram,
+        attribLocations : {
+            vertexPosition : gl.getAttribLocation(shaderProgram, 'aVertexPosition')
+        },
+        uniformLocations : {
+            projectionMatrix : gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+            modelViewMatrix : gl.getUniformLocation(shaderProgram, 'uModelViewMatrix')
+        }
+    };
+
+    const buffers = initBuffer(gl);
+
+    drawScene(gl, programInfo, buffers);
 }
 
 window.onload = main;
